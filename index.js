b1f18f0067d3a47f3884ecd9f6eb34a59657d95f
Alexey Bondarenko <bondarenko@banki.ru>
2022-04-01 20:21:52 +0300

    feat: first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+var _ = require('lodash');
+
+var users = [
+  { 'user': 'barney', 'age': 36, 'active': true },
+  { 'user': 'fred',   'age': 40, 'active': false }
+];
+ 
+var testFindWhere = _.result(_.findWhere(users, { 'age': 36, 'active': true }), 'user');
+// => 'barney'
+
+var users1 = [
+  { 'user': 'barneybarney',  'age': 36, 'active': true },
+  { 'user': 'fred',    'age': 40, 'active': false },
+  { 'user': 'pebbles', 'age': 1,  'active': true }
+];
+ 
+var testFind = _.result(_.find(users1, function(chr) {
+  return chr.age < 40;
+}), 'user');
+// => 'barneybarney'
+
+module.exports.testFindWhere = testFindWhere;
+module.exports.testFind = testFind;
